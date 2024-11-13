4b4ba1b1388f961caa07e73f5ec927c6567a9b53
maseikin.nv <nikolaimaseikin@mail.ru>
2024-11-13 10:23:37 +0300

    Add BottomNavigationBar

--- app/src/main/java/com/maseykin/speakeasy/app/MainActivity.kt
+++ app/src/main/java/com/maseykin/speakeasy/app/MainActivity.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.primarySurface
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.maseykin.speakeasy.app.ui.theme.SpeakEasyTheme
 
@@ -19,11 +31,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpeakEasyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar()
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Translate App Content")
+                    }
                 }
             }
         }
@@ -31,17 +51,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun BottomNavigationBar() {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.primarySurface,
+        contentColor = MaterialTheme.colors.onPrimary
+    ) {
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    ImageVector.vectorResource(id = R.drawable.ic_mic),
+                    contentDescription = "Chat"
+                )
+            },
+            selected = false,
+            onClick = {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpeakEasyTheme {
-        Greeting("Android")
+            }
+        )
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    ImageVector.vectorResource(id = R.drawable.ic_camera),
+                    contentDescription = "Camera"
+                )
+            },
+            selected = false,
+            onClick = {
+
+            }
+        )
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    ImageVector.vectorResource(id = R.drawable.ic_translate),
+                    contentDescription = "Translate"
+                )
+            },
+            selected = false,
+            onClick = {
+
+            }
+        )
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    ImageVector.vectorResource(id = R.drawable.ic_history),
+                    contentDescription = "History"
+                )
+            },
+            selected = false,
+            onClick = {
+
+            }
+        )
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    ImageVector.vectorResource(id = R.drawable.ic_fav),
+                    contentDescription = "Favorite"
+                )
+            },
+            selected = false,
+            onClick = {
+
+            }
+        )
     }
 }
